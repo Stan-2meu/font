0e2a37ced7f593751a8ff82ce4870db19f5e43ea
stan <stan@2meu.me>
2024-02-26 11:56:37 +0900

    add native noto sans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
+import {Noto_Sans} from "next/font/google";
 
 
 const pretendard = localFont({
@@ -19,9 +20,11 @@ const notoSans = localFont({
   preload: true,
 });
 
+const n = Noto_Sans({subsets:["latin"]});
+
 export default function App({ Component, pageProps }: AppProps) {
   return <main
-  className={notoSans.className}
+  className={n.className}
   >
 <Component {...pageProps} />;
   </main> 
